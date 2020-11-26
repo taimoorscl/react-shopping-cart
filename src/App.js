884a152a97e92bdc4cded6daacfac55b0814ea43
TaimoorSourcecode <taimoor@sourcecodelabs.co.uk>
2020-11-26 20:04:22 +0000

    added cart component

--- src/App.js
+++ src/App.js
@@ -1,71 +1,114 @@
-import React, { Component } from 'react'
-import data from './data.json'
-import Products from './components/products'
-import Filter from './components/filter'
+import React, { Component } from "react";
+import data from "./data.json";
+import Products from "./components/products";
+import Filter from "./components/filter";
+import Cart from "./components/cart";
 
-
-class  App extends Component {
-  constructor(){
+class App extends Component {
+  constructor() {
     super();
-    this.state={
+    this.state = {
       products: data.products,
+      cartItems: [],
       size: "",
-      sort: ""
-    }
+      sort: "",
+    };
   }
 
-  sortProducts =(event) =>{
+  sortProducts = (event) => {
     const sort = event.target.value;
-    console.log(event.target.value)
+    console.log(event.target.value);
     this.setState((state) => ({
       sort: sort,
-      products: this.state.products.slice().sort((a,b)  => (
-        sort === "lowest" ?
-        ((a.price > b.price) ? 1 : -1) :
-        sort === "highest" ? 
-        ((a.price < b.price) ? 1 : -1) :
-        ((a._id < b._id) ? 1 : -1)
-      ))
-    }))
-  }
+      products: this.state.products
+        .slice()
+        .sort((a, b) =>
+          sort === "lowest"
+            ? a.price > b.price
+              ? 1
+              : -1
+            : sort === "highest"
+            ? a.price < b.price
+              ? 1
+              : -1
+            : a._id < b._id
+            ? 1
+            : -1
+        ),
+    }));
+  };
 
-  filterProducts=(event => {
+  filterProducts = (event) => {
     console.log(event.target.value);
     if (event.target.value === "") {
       this.setState({
-        size: event.target.value, products: data.products
-      })
+        size: event.target.value,
+        products: data.products,
+      });
     } else {
       this.setState({
-        size: event.target.value, 
+        size: event.target.value,
         products: data.products.filter(
-          (product) => product.availableSizes.indexOf(event.target.value) >= 0 )
-      })
+          (product) => product.availableSizes.indexOf(event.target.value) >= 0
+        ),
+      });
     }
-  })
+  };
+
+  addToCart = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    let alreadyInCart = false;
+    cartItems.forEach((item) => {
+      if (item._id === product._id) {
+        item.count++;
+        alreadyInCart = true;
+      }
+    });
+    if (!alreadyInCart) {
+      cartItems.push({ ...product, count: 1 });
+    }
+    this.setState({ cartItems: cartItems });
+  };
+
+  removeFromCart = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    this.setState({
+      cartItems: cartItems.filter((x) => x._id !== product._id),
+    });
+  };
+
   render() {
-  return (
-    <div className="grid-container">
-      <header>
-        <a href='/'>React Shopping cart</a>
-      </header>
-      <main>
-        <div className="content">
-          <div className="main">
-            <Filter count={this.state.products.length} 
-            size={this.state.size}
-            sort={this.state.sort}
-            sortProducts={this.sortProducts}
-            filterProducts={this.filterProducts}
-            />
-            <Products products={this.state.products}/>
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/">React Shopping cart</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Filter
+                count={this.state.products.length}
+                size={this.state.size}
+                sort={this.state.sort}
+                sortProducts={this.sortProducts}
+                filterProducts={this.filterProducts}
+              />
+              <Products
+                products={this.state.products}
+                addToCart={this.addToCart}
+              />
+            </div>
+            <div className="sidebar">
+              <Cart
+                cartItems={this.state.cartItems}
+                removeFromCart={this.removeFromCart}
+              />
+            </div>
           </div>
-          <div className="sidebar">cartitems</div>
-        </div>
-      </main>
-      <footer>All rigth is reserved</footer>
-    </div>
-  );
+        </main>
+        <footer>All rigth is reserved</footer>
+      </div>
+    );
   }
 }
 
