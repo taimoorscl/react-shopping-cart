db6bfc473a817b32b14462b4394107dd052470a0
TaimoorSourcecode <taimoor@sourcecodelabs.co.uk>
2020-11-26 09:57:46 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href='/'>React Shopping cart</a>
+      </header>
+      <main>
+        Product List
+      </main>
+      <footer>All rigth is reserved</footer>
+    </div>
+  );
+}
+
+export default App;
